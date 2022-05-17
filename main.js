241bf9643102c97d6e86dd7f0b0d6a5c487ab3ba
Roman <105034544+romanskr27@users.noreply.github.com>
2022-05-17 21:25:01 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
+// Задание №1
 function showAlert() {
     alert("Hello Artem");
 }
 var el = document.getElementById("knopka");
 el.addEventListener("click", showAlert, false);
+
+// Задание №2
+function changeText() {
+    span.textContent = "Text changed!!!";
+}  
+
+var span = document.getElementById("textField");
+              
+             
